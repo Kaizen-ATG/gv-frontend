9ae841e4198aa9603f9cd1ec4d4758b2f03dcd4d
JasonM0rris <jmail.morris@gmail.com>
2021-01-04 21:28:30 +0000

    menu dropdown color changed

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -8,7 +8,7 @@ export const SidebarContainer = styled.aside`
   z-index: 999;
   width: 100%;
   height: 100%;
-  background: #0d0d0d;
+  background: #fff;
   display: grid;
   align-items: center;
   top: 0;
@@ -20,7 +20,7 @@ export const SidebarContainer = styled.aside`
 `;
 
 export const CloseIcon = styled(FaTimes)`
-  color: #fff;
+  color: #0d0d0d;
 `;
 
 export const Icon = styled.div`
@@ -34,7 +34,7 @@ export const Icon = styled.div`
 `;
 
 export const SidebarWrapper = styled.div`
-  color: #fff;
+  color: #0d0d0d;
 
   text-align: center;
 `;
@@ -58,7 +58,7 @@ export const SidebarLink = styled(LinkS)`
   list-style: none;
   transition: 0.2s ease-in-out;
   text-decoration: none;
-  color: #fff;
+  color: #0d0d0d;
   cursor: pointer;
 
   &:hover {
@@ -77,7 +77,7 @@ export const SidebarRoute = styled(LinkR)`
   background: #01bf71;
   white-space: nowrap;
   padding: 16px 64px;
-  color: #010606;
+  color: #fff;
   font-size: 16px;
   outline: none;
   border: none;
@@ -89,5 +89,6 @@ export const SidebarRoute = styled(LinkR)`
     transition: all 0.2s ease-in-out;
     background: #fff;
     color: #010606;
+    border: 1px solid;
   }
 `;
