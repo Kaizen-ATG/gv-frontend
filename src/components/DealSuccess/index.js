eba3caaa3c7f40a00f9ac1943243e255e62b7b1e
JasonM0rris <jmail.morris@gmail.com>
2021-02-04 20:40:09 +0000

    Added success animation to pages

--- src/components/DealSuccess/index.js
+++ src/components/DealSuccess/index.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 // import { Button } from "../ButtonElement";
+import lottie from "lottie-web";
+import animationData from "./coupon.json";
 import {
   ContentH1,
   Text,
   ContentWrap,
-  ImageWrapper,
   Container,
   CodeText,
   CodeTextContainer,
@@ -18,11 +19,27 @@ const DealSuccessSection = () => {
   //     setHover(!hover);
   //   };
 
+  const container = useRef(null);
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container.current,
+      renderer: "svg",
+      loop: false,
+      autoplay: true,
+      animationData: animationData,
+    });
+  }, []);
+
   return (
     <>
       <Container>
         <ContentWrap>
-          <ImageWrapper src="/images/elements/Coupon.svg" alt="Success" />
+          <div
+            className="container"
+            ref={container}
+            style={{ width: 300, margin: "0 auto" }}
+          ></div>
           <ContentH1>Woo Hoo!</ContentH1>
           <Text>Here's your unique code</Text>
           <CodeTextContainer>
