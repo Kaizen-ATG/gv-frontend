355f1ad2a937298a94c20dc2ec49fcd794b66a8d
JasonM0rris <jmail.morris@gmail.com>
2021-02-08 23:47:27 +0000

    Animation added to Add points and Redeem offers pages

--- src/components/AddPoints/AddPointsForm.js
+++ src/components/AddPoints/AddPointsForm.js
@@ -1,5 +1,8 @@
+import React, { useEffect, useRef } from "react";
 import useForm from "./useForm";
 import validate from "./validateInfo";
+import lottie from "lottie-web";
+import animationData from "./add-points.json";
 
 import {
   Container,
@@ -12,7 +15,6 @@ import {
   InfoContainer,
   FormLabel,
   Information,
-  ImageWrapper,
   ButtonWrapper,
   ApplyButton,
 } from "./AddPointElements";
@@ -23,16 +25,29 @@ const AddPointsForm = ({ submitForm }) => {
     validate
   );
 
+  const container = useRef(null);
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: animationData,
+    });
+  }, []);
+
   return (
     <>
       <Container>
         <FormWrap>
           <FormContent>
             <Form onSubmit={handleSubmit}>
-              <ImageWrapper
-                src="/images/characters/enter-code.svg"
-                alt="enter code"
-              />
+              <div
+                className="container"
+                ref={container}
+                style={{ width: 300, margin: "0 auto" }}
+              ></div>
               <FormH1>Enter Your Code </FormH1>
               <FormLabel htmlFor="code">
                 Enter the 4 digit code as seen on the kiosk screen/ receipt
